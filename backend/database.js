d21f54582b0a274f3e2ca7eaca523d81e2541e47
Samir Muhammad <63614993+smuh27@users.noreply.github.com>
2024-06-04 01:40:53 -0400

    backend/DB set up - roster table added

---
+++ backend/database.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+
+const express = require('express');
+const { Client } = require('pg');
+const cors = require('cors');
+
+const app = express();
+const port = 5000;
+
+
+app.use(express.json());
+app.use(cors());
+
+const client = new Client({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,  
+    database: process.env.DB_NAME
+});
+
+client.connect();
+
+app.get('/api/players', (req, res) => {
+    client.query('SELECT * FROM "Players"', (err, result) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.json(result.rows);
+        }
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
--- src/pages/Roster.js
+++ src/pages/Roster.js
@@ -1,3 +1,42 @@
-export default function Rostert() {
-    return <h1> Roster </h1>
-}
+import React, { useEffect, useState } from 'react';
+import '../styles/pages.css'; 
+import axios from 'axios';
+
+const Roster = () => {
+    const [players, setPlayers] = useState([]);
+    useEffect(() => {
+        // Fetch data from backend
+        axios.get('http://localhost:5000/api/players')
+            .then(response => {
+                setPlayers(response.data);
+            })
+            .catch(error => {
+                console.error("There was an error fetching the players!", error);
+            });
+    }, []);
+    return (
+        <div className='table-container'>
+            <table className='styled-table'>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Position</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {players.map(player => (
+                        <tr key={player.id}>
+                            <td>{player.id}</td>
+                            <td>{player.name}</td>
+                            <td>{player.position}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+    
+};
+
+export default Roster;
